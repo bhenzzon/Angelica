f5e4152d7e7dec976cf10618df60be9e057aba90
Bhenzzonsaure14@gmail.com <Bhenzzonsaure14@gmail.com>
2024-03-07 23:45:51 +0800

    commit

--- config.php
+++ config.php
@@ -11,6 +11,6 @@ $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
 // Check connection
 if($mysqli === false){
-    die("ERROR: Could not connect. " . $mysqli->connect_error);
+    die("ERROR: Could not connect. " .  $mysqli_connect_error);
 }
 ?>
