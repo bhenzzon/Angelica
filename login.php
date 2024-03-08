68b74d6d27f5fda9fd88d02c223180f6fa0aad47
bhenzzon <126250682+bhenzzon@users.noreply.github.com>
2024-03-08 13:30:05 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,81 +1,80 @@
-
- <?php
+<?php
 // Initialize the session
 session_start();
- 
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: login.php");
+
+// Check if the user is already logged in, if yes then redirect him to the welcome page
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+    header("location: index.php");
     exit;
 }
- 
+
 // Include config file
 require_once "config.php";
- 
+
 // Define variables and initialize with empty values
 $username = $password = "";
 $username_err = $password_err = $login_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
+
+// Processing form data when the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    // Check if the username is empty
+    if (empty(trim($_POST["username"]))) {
         $username_err = "Please enter username.";
-    } else{
+    } else {
         $username = trim($_POST["username"]);
     }
-    
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
+
+    // Check if the password is empty
+    if (empty(trim($_POST["password"]))) {
         $password_err = "Please enter your password.";
-    } else{
+    } else {
         $password = trim($_POST["password"]);
     }
-    
+
     // Validate credentials
-    if(empty($username_err) && empty($password_err)){
+    if (empty($username_err) && empty($password_err)) {
         // Prepare a select statement
         $sql = "SELECT id, username, password FROM users WHERE username = ?";
-        
-        if($stmt = $mysqli->prepare($sql)){
+
+        if ($stmt = $mysqli->prepare($sql)) {
             // Bind variables to the prepared statement as parameters
             $stmt->bind_param("s", $param_username);
-            
+
             // Set parameters
             $param_username = $username;
-            
+
             // Attempt to execute the prepared statement
-            if($stmt->execute()){
+            if ($stmt->execute()) {
                 // Store result
                 $stmt->store_result();
-                
-                // Check if username exists, if yes then verify password
-                if($stmt->num_rows == 1){                    
+
+                // Check if the username exists, if yes then verify the password
+                if ($stmt->num_rows == 1) {
                     // Bind result variables
                     $stmt->bind_result($id, $username, $hashed_password);
-                    if($stmt->fetch()){
-                        if(password_verify($password, $hashed_password)){
+                    if ($stmt->fetch()) {
+                        if (password_verify($password, $hashed_password)) {
                             // Password is correct, so start a new session
                             session_start();
-                            
+
                             // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;                            
-                            
-                            // Redirect user to welcome page
+                            $_SESSION["username"] = $username;
+
+                            // Redirect the user to the welcome page
                             header("location: index.php");
-                        } else{
+                        } else {
                             // Password is not valid, display a generic error message
                             $login_err = "Invalid username or password.";
                         }
                     }
-                } else{
+                } else {
                     // Username doesn't exist, display a generic error message
                     $login_err = "Invalid username or password.";
                 }
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
@@ -83,129 +82,124 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $stmt->close();
         }
     }
-    
+
     // Close connection
     $mysqli->close();
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
-      
-        
-       
-       #logo{ 
-       position: center; 
-       border-radius: 50%;
-       top:0; 
-       left:0; 
-       } 
+        #logo {
+            position: center;
+            border-radius: 50%;
+            top: 0;
+            left: 0;
+        }
 
-       .wrapper{
-        position: center;
+        .wrapper {
+            position: center;
+        }
 
-       }
-       
-       body{
-       background-image: url(purple.jpg);
-       background-position: center;
-       .wrapper{ width: 360px; padding: 20px; }
-       }
-       
-       #form{
-       background-color: #f1a3ad;
-       width:27%;
-       border-radius: 4px;
-       margin:120px auto;
-       padding:px;
-       
-       }
+        body {
+            background-image: url(purple.jpg);
+            background-position: center;
+        }
 
-       body{
-       background-image: url("img/make5.jpg");
-       backdrop-filter: blur(5px);
-       }
+        #form {
+            background-color: #f1a3ad;
+            width: 27%;
+            border-radius: 4px;
+            margin: 120px auto;
+            padding: px;
+        }
 
-       img {
-  border-radius: 50%;
-  position: center;
-}
+        body {
+            background-image: url("img/make5.jpg");
+            backdrop-filter: blur(5px);
+        }
 
-img:hover {
-  box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
-}
+        img {
+            border-radius: 50%;
+            position: center;
+        }
 
-.h2 {
-    font-family: 'Brush Script MT', calligrapher;
-    font-style: bold;
-}
+        img:hover {
+            box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
+        }
 
-       
-       #btn{
-       color:rgb(255, 255, 255);
-       background-color: rgb(189, 22, 22);
-       padding:10px;
-       font-size: large;
-       border-radius: 10px;
-       }
-       
-       @media screen and (max-width: 570px) {
-       #form {
-         width: 65%;
-         margin-left:none;
-         padding:40px;
-       }
-       
-       
-       
-       }
+        .h2 {
+            font-family: 'Brush Script MT', calligrapher;
+            font-style: bold;
+        }
 
-       </style>
+        #btn {
+            color: rgb(255, 255, 255);
+            background-color: rgb(189, 22, 22);
+            padding: 10px;
+            font-size: large;
+            border-radius: 10px;
+        }
+
+        @media screen and (max-width: 570px) {
+            #form {
+                width: 65%;
+                margin-left: none;
+                padding: 40px;
+            }
+        }
+    </style>
 </head>
+
 <body>
     <div id="form">
-    <div class="wrapper">
-    <center><a href="index.html">
-  <img src="img/ag.png" alt="ag">
-</a> </center>
-    
-<h2 class="h2">Login here</h2>
-        <p>Please fill in your credentials to login.</p>
+        <div class="wrapper">
+            <center><a href="index.html">
+                    <img src="img/ag.png" alt="ag">
+                </a> </center>
 
-        <?php 
-        if(!empty($login_err)){
-            echo '<div class="alert alert-danger">' . $login_err . '</div>';
-        }        
-        ?>
+            <h2 class="h2">Login here</h2>
+            <p>Please fill in your credentials to login.</p>
 
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>Username</label>
-                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
-                <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
-            <div class="form-group">
-                <label>Password</label>
-                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
-                <span class="invalid-feedback"><?php echo $password_err; ?></span>
-            </div>
-            <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
-            </div>
-            <p>Don't have an account? <a href="signup.php">Sign up now</a>.</p>
-            <li>
-                <a href="index.html">
-                    <i class="fas fa-sign out"></i>
-                    Back to home
-                </a>
-            </li>
-        </form>
+            <?php
+            if (!empty($login_err)) {
+                echo '<div class="alert alert-danger">' . $login_err . '</div>';
+            }
+            ?>
 
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                    <span class="invalid-feedback"><?php echo $username_err; ?></span>
+                </div>
+                <div class="form-group">
+                    <label>Password</label>
+                    <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>">
+                    <span class="invalid-feedback"><?php echo $password_err; ?></span>
+                </div>
+                <div class="form-group">
+                    <input type="submit" class="btn btn-primary" value="Login">
+                </div>
+                <p>Don't have an account? <a href="signup.php">Sign up now</a>.</p>
+
+                <ul>
+                    <li>
+                        <a href="index.html">
+                            <i class="fas fa-sign-out"></i> Back to home
+                        </a>
+                    </li>
+                </ul>
+            </form>
+
+        </div>
     </div>
 </body>
+
 </html>
